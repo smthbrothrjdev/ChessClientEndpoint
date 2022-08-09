8ec446b34548e30de54ab8d169da5f6f5040793f
Action Jackson <smthbrothrjdev@gmail.com>
2022-08-09 15:10:54 -0500

    Add isempty method and peice argument to constructor of Square calss

--- ChessClientEndpoint.java
+++ ChessClientEndpoint.java
@@ -3,6 +3,7 @@ public class ChessClientEndpoint {
     public static void main(String[] args){
 
         System.out.println("function check");
-
+        Square s = new Square('r', 2);
+        System.out.println(s.isEmpty());
     }
 }
--- Square.java
+++ Square.java
@@ -1,7 +1,7 @@
 public class Square {
     char rank;
     int file;
-    String occupiedPiece= "";
+    Piece occupiedPiece;
 
     public Square(){
 
@@ -11,5 +11,15 @@ public class Square {
         this.rank = r;
         this.file = f;
     }
+
+    public Square( char r, int f, Piece p){
+        this.rank =r;
+        this.file =f;
+        this.occupiedPiece = p;
+    }
     //TODO add constructor for with peice and replace strin peice with object piece.
+
+    public boolean isEmpty(){
+        return (occupiedPiece == null);
+    }
 }
